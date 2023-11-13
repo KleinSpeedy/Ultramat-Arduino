61f335e6b4fd58f52d566aa77305ccd8e7f85443
jschulze <joni.schulze@t-online.de>
2023-11-13 17:17:18 +0100

    Add files for serial communication handling

---
+++ include/Communicator.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Communicator
+{
+public:
+
+    Communicator();
+
+    bool start();
+
+private:
+
+};
---
+++ src/Communicator.cpp
@@ -0,0 +1,12 @@
+// module include
+#include "Communicator.h"
+
+Communicator::Communicator()
+{
+
+}
+
+bool Communicator::start()
+{
+    return false;
+}
