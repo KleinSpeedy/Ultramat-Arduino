7ff33c57472fa36ad248c18066b60136b371e2df
jschulze <joni.schulze@t-online.de>
2023-11-13 17:19:02 +0100

    Implement atmega328p MCU init by copying Arduino init func

---
+++ include/util/Device.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// project includes
+
+// system includes
+
+namespace Device {
+
+/*
+ * @brief This function is a copy of the init() func of Arduino
+ */
+void initMcu();
+
+} // namespace Device
